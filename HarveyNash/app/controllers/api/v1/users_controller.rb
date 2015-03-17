3a93d05cab45080c2cd2b1139fa826684b6fd62c
Travis Nesland <tnesland@gmail.com>
2015-03-17 15:56:16 -0400

    After registration, user is logged in and user id and auth_token are returned.

--- HarveyNash/app/controllers/api/v1/users_controller.rb
+++ HarveyNash/app/controllers/api/v1/users_controller.rb
@@ -20,7 +20,8 @@ module Api
       def create
         @user = User.new(user_params)
         if @user.save
-          render json: { message: "User was registered" }, status: 200
+          render json: { id: @user.id, auth_token: @user.generate_auth_token },
+            status: 200
         else
           render json: { :errors => @user.errors.as_json }, status: 406
         end
